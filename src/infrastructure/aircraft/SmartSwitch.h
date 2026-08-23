b02320a66dd7b14ee3737cf02c4b1135bd2a0083
Bruno Magalhães <brunom@tuta.io>
2026-08-23 17:04:23 -0300

    fix: the smart switch throws away the span a paused sim stretched

--- src/infrastructure/aircraft/SmartSwitch.h
+++ src/infrastructure/aircraft/SmartSwitch.h
@@ -4,6 +4,7 @@
 #include <functional>
 #include <optional>
 #include <string>
+#include <utility>
 #include <vector>
 
 class VariableGateway;
@@ -19,11 +20,17 @@ public:
     void Subscribe();
     [[nodiscard]] bool Consume();
 
+    void SetClockForTest(std::function<long long()> clock) { nowMs_ = std::move(clock); }
+
 private:
+    static constexpr long long kMaxGapMs = 3000;
+
     VariableGateway& gateway_;
     std::vector<std::string> lvars_;
     Predicate pressed_;
     std::optional<double> resetTo_;
+    std::function<long long()> nowMs_;
+    long long lastConsumeMs_ = 0;
     bool subscribed_ = false;
     bool pending_ = false;
 };
