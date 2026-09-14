f13a31bff9ca49ebab7fccbbf762c1801f974480
Bruno Magalhães <brunom@tuta.io>
2026-09-14 13:09:30 -0300

    feat(turnaround): flag a plan whose operating empty weight leaves out the crew

--- src/domain/model/AutomationStatus.h
+++ src/domain/model/AutomationStatus.h
@@ -21,6 +21,7 @@ struct AutomationStatus
     bool fuelRequestStalled = false;
     bool fuelPlanOverCapacity = false;
     bool fuelDidNotStay = false;
+    bool planOmitsCrew = false;
     bool servicesStalled = false;
     bool serviceInterrupted = false;
     CargoLoader loaderHoldingBoarding = CargoLoader::None;
@@ -35,6 +36,8 @@ struct AutomationStatus
     double plannedZfwKg = 0.0;
     double plannedOperatingEmptyKg = 0.0;
     std::optional<double> plannedPayloadKg;
+    double omittedCrewKg = 0.0;
+    double operatingEmptyWithCrewKg = 0.0;
     int plannedPassengers = 0;
     int boardedPassengers = 0;
     double targetFuelKg = 0.0;
