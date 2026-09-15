b620796d5af50bbe5e29b539e07a254767dc3995
Bruno Magalhães <brunom@tuta.io>
2026-09-15 18:02:56 -0300

    refactor(boarding): let the boarding state own the loader door deadline and its countdown

--- src/domain/turnaround/TurnaroundData.h
+++ src/domain/turnaround/TurnaroundData.h
@@ -38,6 +38,7 @@ struct TurnaroundData
     CargoLoader loaderAwaitingDoor = CargoLoader::None;
     CargoLoader loaderHoldingBoarding = CargoLoader::None;
     int loaderDoorWaitTicks = 0;
+    int loaderDoorWaitSeconds = 0;
     bool deboardingBaselined = false;
     bool refuelingRequested = false;
     int fuelRequestStallTicks = 0;
